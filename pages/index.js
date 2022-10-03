8108d7df274bd81dc372343fe754bcb69e421583
ejaltuna <ealtunae@gmail.com>
2022-10-03 11:00:16 -0400

    ac

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Safs
+          
         </h1>
 
         <p className={styles.description}>
